ed9096541f16f98ccdb517822a3913d257d18589
qw-jl <joseluis@quitiweb.com>
2020-01-29 17:15:23 +0100

    Updates

---
+++ precache-manifest.24135d15dc9980aa247af8001b3d2510.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "364797f76012ec416130224f7dc84af3",
+    "url": "/shopping-list/index.html"
+  },
+  {
+    "revision": "59752d880a6af2e510b8",
+    "url": "/shopping-list/static/css/main.9b1d3eaf.chunk.css"
+  },
+  {
+    "revision": "bb476cade6ab03cc7201",
+    "url": "/shopping-list/static/js/2.a793ddaa.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/shopping-list/static/js/2.a793ddaa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "59752d880a6af2e510b8",
+    "url": "/shopping-list/static/js/main.ffa9a6fb.chunk.js"
+  },
+  {
+    "revision": "f9f63649e56ecc6356c5",
+    "url": "/shopping-list/static/js/runtime-main.bbd521f6.js"
+  }
+]);
